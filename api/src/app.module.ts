2920e60eba4b93ad4408d574bd4ebceda47ff3f8
Ricardo Trejo Sanjuan <ricardo.trejo@stratplus.net>
2020-10-01 17:41:31 -0500

    🐛 BUG

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -4,15 +4,17 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductModule } from './product/product.module';
 
-const conexion = `mongodb+srv://${process.env.MDB_USER}:${process.env.MDB_PASS}@${process.env.MDB_HOST}/${process.env.MDB_DATABASE}?retryWrites=true&w=majority`;
+const user = process.env.MDB_USER || 'testuser';
+const pass = process.env.MDB_PASS || '4APWK5m^CuT8';
+const host = process.env.MDB_HOST || 'guiltyspark.qsbai.mongodb.net';
+const database = process.env.MDB_DATABASE || 'RoomieTest';
+const conexion = `mongodb+srv://${user}:${pass}@${host}/${database}?retryWrites=true&w=majority`;
 console.log('conexion :', conexion);
 
 @Module({
   imports: [
     ProductModule,
     MongooseModule.forRoot(conexion, {
-    // MongooseModule.forRoot('mongodb+srv://testuser:4APWK5m^CuT8@cluster0.czd5z.mongodb.net/Tienda?retryWrites=true&w=majority', {
-    // MongooseModule.forRoot('mongodb+srv://testuser:4APWK5m^CuT8@guiltyspark.qsbai.mongodb.net/RoomieTest?retryWrites=true&w=majority', {
       useCreateIndex: true,
       useNewUrlParser: true
     })
